c70c870c46fb2b09ca6db4179ecede3dde6e43eb
mud-ali <mudali25@bergen.org>
2022-08-18 16:53:33 -0400

    delays and work on share feature

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,7 @@ const trs = Array.from(document.querySelectorAll('tr'));
 let attempt = 0;
 let letter = 0;
 let currentWord = "";
+let colorGridPattern = [];
 
 document.addEventListener('keyup', (e)=>{
     const tds = Array.from(trs[attempt].getElementsByTagName('td'));
@@ -37,36 +38,70 @@ document.addEventListener('keyup', (e)=>{
     }
 });
 
-function checkWin(attempted, cells) {
+async function checkWin(attempted, cells) {
     let cor = 0;
+    let colGridRow = [];
 
     for (let cell=0;cell<cells.length;cell++) {
         
         
+        await sleep(170);
         if (attempted.charAt(cell) == word.charAt(cell)) {
-            setTimeout(()=>{
-                cells[cell].style.backgroundColor = "green"
-            }, 500);
+            cells[cell].style.backgroundColor = "green"
+            colGridRow.push("g");
             cor++;
         }
         else if (word.split("").includes(attempted.charAt(cell))) {
-            setTimeout(()=>{
-                cells[cell].style.backgroundColor = "#f5da2f"; //yellow
-            }, 500);
+            colGridRow.push("y");
+            cells[cell].style.backgroundColor = "#f5da2f"; 
         }
         else if (attempted.charAt(cell) != word.charAt(cell)) {
-            setTimeout(()=>{
-                cells[cell].style.backgroundColor = "#575757"; //grey
-            }, 500);
+            colGridRow.push("b");
+            cells[cell].style.backgroundColor = "#575757";
         }  
         
-        if (cor==5) setTimeout(alert(attempt), 1000);
+        if (cor==5) {
+            setTimeout(alert("You won in "+attempt+" attempts\nClick the screen to copy your results"), 400);
+            genShare();
+        }
     }
+
+    colorGridPattern.push(colGridRow);
 }
 
 function getWord() {
-    const start = new Date("03/26/2022");
+    const start = new Date("03/23/2022");
     const today = new Date();
     let diff = Math.floor((today.getTime() - start.getTime()) / (1000*3600*24));
     return realwords[diff];
+}
+
+function genShare() {
+    console.log(colorGridPattern);
+    let share = "Wordel "+realwords.indexOf(word);
+    share += " "+attempt+"/6\n\n"; ;
+    for (let i=0;i<colorGridPattern.length;i++) {
+        let row = colorGridPattern[i];
+        for (let j=0;j<row.length;j++) {
+            share += row[j]=="g"?"🟩":row[j]=="y"?"🟨":"⬛";
+        }
+        share += "\n";
+    }
+    share += "🟩🟩🟩🟩🟩\n";
+
+    document.body.onclick = () => {
+        navigator.clipboard.writeText(share).then(function() {
+        }, function(err) {
+            throw err;
+        });
+        document.body.onclick = null;
+    }
+
+}
+function sleep(period) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, period);
+    });
 }
--- README.md
+++ README.md
@@ -2,17 +2,4 @@
 
 ### My own Wordle clone
 
-<<<<<<< HEAD
-Working on adding a keyboard :)
-=======
-Bugs
-  - None that I've noticed so far
->>>>>>> dcb78f93a862d08fb15ef3907538a54f21b53f5c
-
-Looking to add a few more features
-  - animations
-  - winning moment
-  - losing moment
-  - share button
-  - colored feedback on keyboard
-  - additional spin to make it less of a clone and more *unique*
+- keyboard in progress
