b0036f03f9ba477c340b0b40ffa8cf2489b42db1
mud-ali <mudali25@bergen.org>
2022-08-27 20:35:38 -0400

    feat.: added keyboard with styles + functionality

--- JS/kboard.js
+++ JS/kboard.js
@@ -1,11 +1,40 @@
 const trs = document.getElementsByClassName('tr');
-let keys = ["QWERTYUIOP","ASDFGHJKL","ZXCVBNM"];
-for (i=0;i<trs.length;i++) {
-    for (j=0;j<keys[i].length;j++) {
-        let td = document.createElement("div");
-        td.classList.add("td");
-        //message parent frame
-        td.innerText = keys[i].charAt(j);
-        trs[i].appendChild(td);
+let parent;
+
+window.addEventListener("message", (e) => {
+    if (e.data === "load") {
+        parent = e.source;
+    } else {
+        // console.log(`main.js sent: ${e.data}.\nsearching class letter-${e.data.split(".")[0]}`);
+        document.getElementsByClassName(`letter-${e.data.split(".")[0].toUpperCase()}`)[0].style.backgroundColor = e.data.split(".")[1];
     }
-}
+}, false);
+
+document.addEventListener('keyup', (e)=>{
+    parent.postMessage(e.key, "*");
+});
+
+(function setup(){
+    let keys = ["QWERTYUIOP","ASDFGHJKL","ZXCVBNM"];
+    for (i=0;i<trs.length;i++) {
+        for (j=0;j<keys[i].length;j++) {
+            let td = document.createElement("div");
+            td.classList.add("td");
+            td.classList.add(`letter-${keys[i].charAt(j)}`);
+            td.innerText = keys[i].charAt(j);
+            td.addEventListener("click", (e)=>{
+                parent.postMessage(e.target.innerText, "*");
+            });
+            trs[i].appendChild(td);
+        }
+    }
+    
+    const otherButtons = document.getElementsByClassName("other-button");
+    for (i=0;i<otherButtons.length;i++) {
+        otherButtons[i].addEventListener("click", (e)=>{
+            console.log(e.target.classList[0]);
+            parent.postMessage(e.target.classList[0]=="Enter"?"Enter":"Backspace", "*");
+        });
+    }
+
+})()
--- JS/main.js
+++ JS/main.js
@@ -1,71 +1,92 @@
 const word = getWord(); //'shout';
 const table = document.querySelector('table');
 const trs = Array.from(document.querySelectorAll('tr'));
+const keyboard = document.getElementsByTagName("iframe")[0];
 let attempt = 0;
 let letter = 0;
 let currentWord = "";
 let colorGridPattern = [];
 
-document.addEventListener('keyup', (e)=>{
+window.addEventListener("load", () => {
+    keyboard.contentWindow.postMessage("load", "*");
+});
+window.addEventListener("message", async (e) => {
+    if (e.data.length == 1 || e.data == "Enter" || e.data == "Backspace") {
+        console.log("main.js: "+e.data);
+        await handleInput(e.data);
+    }
+}, false);
+
+document.addEventListener('keyup', async (e)=>{
+    await handleInput(e);
+});
+
+async function handleInput(e) {
+    let key = (e.length == 1 || e=="Enter" || e=="Backspace") ? e.toLowerCase() : e.key;
+    ctrl = e.ctrlKey ? e.ctrlKey : false;
     const tds = Array.from(trs[attempt].getElementsByTagName('td'));
-    if (e.key == "Backspace") {
-        console.log(tds, letter, attempt);
-        console.log(tds[letter]);
+    if (key == "backspace" && letter > 0) {
+        // console.log(tds, letter, attempt);
+        // if it exists, remove the last letter
         tds[letter-1].innerText = "";
         letter--;
         currentWord = currentWord.substring(0, currentWord.length - 1);
         return;
-    } else if (e.key == "Enter" && letter == 5) {
+    } else if (key == "enter" && letter == 5) {
         if (!(allowedWords.includes(currentWord)) && !realwords.includes(currentWord)) {
             alert("Not a valid word");
             return;
         }
-
         attempt++;
-        
-        checkWin(currentWord, tds);
-        //lose condition if attemp ==6
-        currentWord = "";
-        letter=0;
+        await checkWin(currentWord, tds);
+
+        if (attempt == 6) {
+            alert("You lost\nClick the screen ");
+            genShare(false);
+            document.body.replaceWith(document.body.cloneNode(true));
+        }
     }
-    if ((e.key.toLowerCase().match(/[a-z]/g) != null) && (e.key.length <= 1) && (!e.ctrlKey) && (letter!=5)) {
-        tds[letter].innerText = e.key.toUpperCase();
-        currentWord += e.key;
+
+
+    if ((key.toLowerCase().match(/[a-z]/g) != null) && (key.length <= 1) && (!ctrl) && (letter!=5)) {
+        tds[letter].innerText = key.toUpperCase();
+        currentWord += key;
         letter++;
     }
     if (letter >= 5) {
         letter = 5;
     }
-});
+}
 
 async function checkWin(attempted, cells) {
+    currentWord = "";
+    letter = 0;
+    console.log(letter,currentWord);
     let cor = 0;
     let colGridRow = [];
 
     for (let cell=0;cell<cells.length;cell++) {
-        
-        
-        await sleep(170);
-        if (attempted.charAt(cell) == word.charAt(cell)) {
-            cells[cell].style.backgroundColor = "green"
-            colGridRow.push("g");
-            cor++;
-        }
-        else if (word.split("").includes(attempted.charAt(cell))) {
-            colGridRow.push("y");
-            cells[cell].style.backgroundColor = "#f5da2f"; 
-        }
-        else if (attempted.charAt(cell) != word.charAt(cell)) {
-            colGridRow.push("b");
-            cells[cell].style.backgroundColor = "#575757";
-        }  
-        
-        if (cor==5) {
-            setTimeout(alert("You won in "+attempt+" attempts\nClick the screen to copy your results"), 400);
-            genShare();
-        }
+        await sleep(95).then(() => {
+            if (attempted.charAt(cell) == word.charAt(cell)) {
+                colGridRow.push("g");
+                    cells[cell].style.backgroundColor = "green";
+                    keyboard.contentWindow.postMessage(`${attempted.charAt(cell)}.green`, "*");
+                cor++;
+            } else if (word.split("").includes(attempted.charAt(cell))) {
+                colGridRow.push("y");
+                    cells[cell].style.backgroundColor = "#f5da2f";
+                    keyboard.contentWindow.postMessage(`${attempted.charAt(cell)}.#f5da2f`, "*");
+            } else if (attempted.charAt(cell) != word.charAt(cell)) {
+                colGridRow.push("b");
+                    cells[cell].style.backgroundColor = "#575757";
+                    keyboard.contentWindow.postMessage(`${attempted.charAt(cell)}.#575757`, "*");
+            }   
+        }); 
     }
-
+    if (cor===5) {
+        setTimeout(()=>{alert("You won in "+attempt+" attempts\nClick the screen to copy your results")}, 630);
+        genShare();
+    }
     colorGridPattern.push(colGridRow);
 }
 
@@ -76,18 +97,18 @@ function getWord() {
     return realwords[diff];
 }
 
-function genShare() {
+function genShare(win=true) {
     console.log(colorGridPattern);
     let share = "Wordel "+realwords.indexOf(word);
-    share += " "+attempt+"/6\n\n"; ;
-    for (let i=0;i<colorGridPattern.length;i++) {
-        let row = colorGridPattern[i];
+    share += ` ${win?attempt:"X"}/6\n\n`;
+    for (let i=0;i<=colorGridPattern.length;i++) {
+        let row = colorGridPattern[i] ?? [];
         for (let j=0;j<row.length;j++) {
             share += row[j]=="g"?"🟩":row[j]=="y"?"🟨":"⬛";
         }
-        share += "\n";
+        share += i!=colorGridPattern.length ? "\n" : "";
     }
-    share += "🟩🟩🟩🟩🟩\n";
+    share += win ? "🟩🟩🟩🟩🟩": "";
 
     document.body.onclick = () => {
         navigator.clipboard.writeText(share).then(function() {
@@ -98,10 +119,11 @@ function genShare() {
     }
 
 }
-function sleep(period) {
+
+function sleep(ms) {
     return new Promise(resolve => {
         setTimeout(() => {
             resolve();
-        }, period);
+        }, ms);
     });
 }
